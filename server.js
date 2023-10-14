69e0572a296dfa37d8274407f0eeb47225764bc5
santamayengb <19mca8232@cuchd.in>
2023-10-14 21:40:47 +0530

    First socket project

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 80 });
 
 wss.on('connection', (ws, req) => {
   // ... (existing code)
