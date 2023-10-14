502d9d3b5c1f8807ae8f324ddaf4b95e8f73e767
santamayengb <19mca8232@cuchd.in>
2023-10-14 18:26:20 +0530

    Add node_modules to .gitignore

---
+++ server.js
@@ -0,0 +1,37 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws, req) => {
+  // ... (existing code)
+
+  ws.on('message', (data) => {
+    
+
+    // Convert the binary data to a string (assuming it's in UTF-8 encoding)
+    const textData = data.toString('utf8');
+
+    try {
+      // Parse the text data as JSON
+      const jsonData = JSON.parse(textData);
+
+      console.log(`Received JSON: ${JSON.stringify(jsonData)}`);
+
+      // Broadcast the received JSON to all connected clients
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          // Convert the JSON back to a string to send it as a message
+          const jsonString = JSON.stringify(jsonData);
+          client.send(jsonString);
+          console.log(jsonString);
+        }
+      });
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
