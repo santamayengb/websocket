e59c042af5e220dc5e6afc8976f8796371450b04
santamayengb <19mca8232@cuchd.in>
2023-10-14 18:54:33 +0530

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 80 });
+const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws, req) => {
   // ... (existing code)
